abc0ec6014978085e715421021f4a94acb9f5648
Vladimiro Antipin <115135530+VladimiroAntipin@users.noreply.github.com>
2023-03-11 21:03:30 +0300

    Delete DndProvider.jsx

--- src/providers/DndProvider.jsx
+++
@@ -1,8 +0,0 @@
-import { DndProvider as RDndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-
-export const DndProvider = ({ children }) => (
-  <RDndProvider backend={HTML5Backend}>
-    {children}
-  </RDndProvider>
-);
