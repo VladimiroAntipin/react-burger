977ac203c70a3242ab8c5cf2538a5da12c59374b
Vladimiro Antipin <115135530+VladimiroAntipin@users.noreply.github.com>
2023-03-13 18:03:55 +0300

    Delete orderHistory.tsx

--- src/components/OrderHistory/orderHistory.tsx
+++
@@ -1,9 +0,0 @@
-export function OrderHistory() {
-  return (
-    <>
-     <p className="text text_type_main-large text_color_inactive">
-        История заказов будет доступна в следующих спринтах
-     </p>   
-     </>        
-  )
-}
